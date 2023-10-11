6f6debc87498d3c72ab0a89fa96bc7567e5dfe00
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2023-10-10 17:11:52 -0700

    Update __init__.py

--- unit1/isprime/__init__.py
+++ unit1/isprime/__init__.py
@@ -13,10 +13,10 @@ def compiles():
 
 @check50.check(compiles)
 def test1():
-    """handles starting population of 1200"""
-    check50.run("./isprime").stdin("4").stdout("Years: 1").exit(0)
+    """check if 4 is prime"""
+    check50.run("./isprime").stdin("4").stdout("NOT PRIME!").exit(0)
 
 @check50.check(compiles)
 def test2():
-    """rejects invalid populations and then handles population 9"""
-    check50.run("./population").stdin("-5").stdin("3").stdin("9").stdin("5").stdin("18").stdout("Years: 8").exit(0)
+    """check if 5 is prime"""
+    check50.run("./isprime").stdin("5").stdout("PRIME!").exit(0)
