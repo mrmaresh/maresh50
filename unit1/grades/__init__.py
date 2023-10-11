a4d9f53734112e165b2c76081863cb5200a96027
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2023-10-10 17:52:19 -0700

    Update __init__.py

--- unit1/grades/__init__.py
+++ unit1/grades/__init__.py
@@ -14,25 +14,25 @@ def compiles():
 @check50.check(compiles)
 def test1():
     """check a score of 55 """
-    check50.run("./isprime").stdin("55").stdout("You got an F.").exit(0)
+    check50.run("./grades").stdin("55").stdout("You got an F.").exit(0)
 
 @check50.check(compiles)
 def test2():
     """check a score of 60 """
-    check50.run("./isprime").stdin("60").stdout("You got a D.").exit(0)
+    check50.run("./grades").stdin("60").stdout("You got a D.").exit(0)
 
 @check50.check(compiles)
 def test3():
     """check a score of 79 """
-    check50.run("./isprime").stdin("79").stdout("You got a C.").exit(0)
+    check50.run("./grades").stdin("79").stdout("You got a C.").exit(0)
 
 @check50.check(compiles)
 def test4():
     """check a score of 85 """
-    check50.run("./isprime").stdin("85").stdout("You got a B.").exit(0)
+    check50.run("./grades").stdin("85").stdout("You got a B.").exit(0)
 
 @check50.check(compiles)
 def test5():
     """check a score of 100 """
-    check50.run("./isprime").stdin("100").stdout("You got an A.").exit(0)
+    check50.run("./grades").stdin("100").stdout("You got an A.").exit(0)
 
